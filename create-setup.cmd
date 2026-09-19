c47ed94132652fbb771a9fb137668aa11fde0103
Himanshu Singh <Himanshuclub88.com>
2026-09-20 01:11:01 +0530

    forced bypass

--- create-setup.cmd
+++ create-setup.cmd
@@ -1,2 +1,5 @@
 @echo off
-powershell -ExecutionPolicy Bypass -File "C:\venvs\scripts\create-setup.ps1" %*
+
+powershell -NoProfile -Command "Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned -Force"
+
+powershell -NoProfile -ExecutionPolicy Bypass -File "C:\venvs\scripts\create-setup.ps1" %*
